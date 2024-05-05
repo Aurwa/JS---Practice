99ef7d763041a94dabdc892eaec1b813b1ac24ae
Aurwa <zafaraurwa11@gmail.com>
2024-05-05 14:23:11 +0500

    Create dataTypes.js
    DataTypes in JS

---
+++ dataTypes.js
@@ -0,0 +1,19 @@
+/*
+number
+bigInt
+string
+null = standalone value
+undefined
+symbol
+boolean
+
+object
+*/
+
+let name = "Aurwa"
+let age = 18
+let isLoggedIn = false
+let state;
+
+console.log(typeof null)
+console.log(typeof undefined)
