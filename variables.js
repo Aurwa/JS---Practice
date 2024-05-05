7c13c4233c00fb04808f464b3a72169cda42b0fe
Aurwa <zafaraurwa11@gmail.com>
2024-05-05 12:40:18 +0500

    Update variables.js
    Let, const and var

--- variables.js
+++ variables.js
@@ -1,4 +1,13 @@
 const accountId = 144553;
 let accountEmail = "aurwa@gmail.com";
 var accountPass = "12345";
+accountCity = "Jaipur";
 
+// accountId = 2;
+//console.log(accountId);
+
+accountEmail = "aurwa@az.com";
+accountPass = "121212";
+accountCity = "Delhi";
+
+console.table([accountId, accountEmail, accountPass, accountCity]);
