831b680df74761292e577f7a863c9cd0f3ddd6e0
Aurwa <zafaraurwa11@gmail.com>
2024-05-05 16:02:32 +0500

    Create conversionOperations.js
    Conversion Operations in JS

---
+++ conversionOperations.js
@@ -0,0 +1,33 @@
+let score = false
+
+//console.log(typeof score)
+
+let valueInNum = Number(score)
+//console.log(typeof valueInNum)
+//console.log(valueInNum)
+
+// "33" => 33
+// "33abc" => Nan
+// null => 0
+// true =>1 , false => 0
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "hitesh" => true
+
+//console.log(Boolean("hello")
+
+//console.log(String(false));
+//console.log(typeof String(false));
+
+let sample = +'aurwa'           // => Unary plus operator
+//console.log(typeof sample)
+//console.log(sample)
+
+
+console.log(String (123 + 'abc'));
