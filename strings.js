154fb2c57da2310045ff5b3578b51b7358134658
Aurwa <zafaraurwa11@gmail.com>
2024-05-20 16:57:17 +0500

    Create strings.js
    Strings in JavaScript

---
+++ strings.js
@@ -0,0 +1,16 @@
+let name = "Aurwa";
+
+//console.log(typeof name);
+let age = 25;
+
+//console.log(`My name is ${name} and my age is ${age}`);
+
+const gameName = new String('Hiteshhc'); 
+/*
+object of type 'String' => This object can access all porperties of strings.
+
+When you create a string using the new String() constructor, you're creating a string object wrapper around the primitive string value. This means that while the object has access to all the methods and properties of the String object, JavaScript automatically converts it to a primitive string value in most contexts to maintain compatibility with the language's behavior.
+
+So, when you access characters using square brackets [], you're essentially treating it like an array or array-like object, which is a common practice in JavaScript. 
+*/
+console.log(gameName);
