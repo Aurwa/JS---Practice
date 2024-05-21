d968214ec1c603fdae53e4527be4ecff46210874
Aurwa <zafaraurwa11@gmail.com>
2024-05-21 15:02:35 +0500

    Update number_maths.js
    Maths in JS

--- number_maths.js
+++ number_maths.js
@@ -6,7 +6,27 @@ const balance = new Number (100)
 //console.log(balance);
 //console.log(balance.toString().length);
 
+/*
 const num1 = 0.0005678
 const num2 = 0.034567
-console.log(num1.toPrecision(2));
+const num3 = num1.toPrecision(2);
+console.log(num3);
+console.log(typeof num3);
 console.log(num2.toPrecision(3));
+console.log(typeof num1);
+*/
+
+const num1 = 1125.8
+//console.log(num1.toPrecision(3));
+
+const num = 123456;
+//console.log(num.toLocaleString("ar-EG"));
+
+// ************ MATHS IN JS ************
+console.log(Math);
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+
+
