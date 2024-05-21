a61cdf5db922f90116a0f54ab82cf61cff3b07d2
Aurwa <zafaraurwa11@gmail.com>
2024-05-21 12:32:28 +0500

    Update strings_practice.js
    trim() in JavaScript

--- strings_practice.js
+++ strings_practice.js
@@ -1,3 +1,4 @@
+/*
 const str = "The code undefined code code";
 //console.log(str.indexOf("code", 5));
 //console.log(str.lastIndexOf("code", 23));
@@ -7,3 +8,7 @@ const str = "The code undefined code code";
 const firstOcc = str.indexOf("");
 const secondOcc = str.indexOf("", firstOcc + 1);
 console.log(secondOcc)
+*/
+
+const str1 = "    Aurwa  "
+console.log(str1.trim());
