4e50d333b3dcd90b737ec2ed30b647685f4b1e90
Aurwa <zafaraurwa11@gmail.com>
2024-05-21 18:02:45 +0500

    Update date_js.js
    Dates in JS

--- date_js.js
+++ date_js.js
@@ -1,13 +1,29 @@
-const myDate = new Date()
-//console.log(myDate);
-//console.log(typeof myDate);
-const newDate = myDate.toLocaleString();
-// console.log("New Date: ", newDate);
-// console.log(typeof newDate);
 
-// const createdDate = new Date(2023, 0, 23);
-// console.log(createdDate.toDateString());
-// console.log(createdDate.toLocaleString());
+const myDate = new Date()
+console.log(myDate); 
+console.log(typeof myDate);
+const newDate = myDate.toLocaleString("default", {
+    weekday: "long"
+});
+console.log("New Date: ", newDate);
+console.log(typeof newDate);
+
+//const createdDate = new Date(2023, 0, 23);
+console.log(createdDate.toDateString());
+console.log(createdDate.toLocaleString());
 
 const createdDate = new Date("2023-01-14");
-console.log(createdDate.toDateString());
+//console.log(createdDate.toDateString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(createdDate.getTime());
+console.log(Math.floor(myTimeStamp/1000));
+
+const date = new Date()
+console.log(date.getDay());
+
+
+
+
+
