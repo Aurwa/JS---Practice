acff2d13d2f266ea9f27da63f35d1ad763c23a25
Aurwa <zafaraurwa11@gmail.com>
2024-05-21 13:38:52 +0500

    Create number_maths.js
    toPrecision() method

---
+++ number_maths.js
@@ -0,0 +1,12 @@
+const score = 444.56;
+//console.log(score)
+//console.log(score.toPrecision(3));
+
+const balance = new Number (100)
+//console.log(balance);
+//console.log(balance.toString().length);
+
+const num1 = 0.0005678
+const num2 = 0.034567
+console.log(num1.toPrecision(2));
+console.log(num2.toPrecision(3));
