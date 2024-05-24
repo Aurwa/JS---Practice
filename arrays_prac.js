75c71f1a60e1cc8cd0a88445f966f485905d5031
Aurwa <zafaraurwa11@gmail.com>
2024-05-24 12:15:23 +0500

    Update arrays_prac.js
    Arrays Practice

--- arrays_prac.js
+++ arrays_prac.js
@@ -13,10 +13,28 @@
 // console.log(arr2);
 // console.log(arr1);
 
-const arr = [1,2,3,4,5,6]
-const arr1 = arr.slice(1,4)
-console.log(arr1);
-console.log("Original array after slice" , arr);
-const arr2 = arr.splice(1,4)
-console.log(arr2);
-console.log("Original array after splice" , arr);
+// const arr = [1,2,3,4,5,6]
+// const arr1 = arr.slice(1,4)
+// console.log(arr1);
+// console.log("Original array after slice" , arr);
+// const arr2 = arr.splice(1,4)
+// console.log(arr2);
+// console.log("Original array after splice" , arr);
+
+
+// const marvel = ["spiderman", "ironman"]
+// const dc = ["superman", "flash"]
+// marvel.push(dc)
+// console.log(marvel);
+// const heros = marvel.concat(dc)
+// console.log(heros);
+// const heros = [...marvel,...dc]
+// console.log(heros);
+
+//console.log(Array.from("Aurwa"));
+const obj = {
+    name: "Aurwa",
+    age: 25
+}
+const arr = Array.from(Object.values(obj))
+console.log(arr);
