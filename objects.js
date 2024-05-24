b7f319a3ff3a2b01b2aad0de1c5d6069a49c2a32
Aurwa <zafaraurwa11@gmail.com>
2024-05-24 18:51:13 +0500

    Update objects.js
    Object.assign() in JS

--- objects.js
+++ objects.js
@@ -3,18 +3,18 @@
 
 //Object Literals
 
-const mySym = Symbol("key1");
+// const mySym = Symbol("key1");
 
-const JsUser = {
-  name: "Aurwa",
-  "full name": "Aurwa Zafar",
-  age: 25,
-  location: "Islamabad",
-  email: "zafar@gmail.com",
-  [mySym]: "myKey1",
-  isLoggedIn: false,
-  lastLoginDays: ["Saturday", "Monday"],
-};
+// const JsUser = {
+//   name: "Aurwa",
+//   "full name": "Aurwa Zafar",
+//   age: 25,
+//   location: "Islamabad",
+//   email: "zafar@gmail.com",
+//   [mySym]: "myKey1",
+//   isLoggedIn: false,
+//   lastLoginDays: ["Saturday", "Monday"],
+// };
 
 // console.log(JsUser.email);
 // console.log(JsUser["email"]);
@@ -22,13 +22,36 @@ const JsUser = {
 // console.log(JsUser);
 // console.log(JsUser[mySym]);
 
-JsUser.email = "z@gmail.com";
+// JsUser.email = "z@gmail.com";
 //console.log(JsUser);
 //Object.freeze(JsUser);
-JsUser.age = 29;
+// JsUser.age = 29;
 //console.log(JsUser);
-JsUser.greeting = function () {
-  return `Hello User ${JsUser.name}`;
+// JsUser.greeting = function () {
+//   return `Hello User ${JsUser.name}`;
+// };
+
+// console.log(JsUser.greeting());
+
+const tindUser = {};
+tindUser.id = "123abc";
+tindUser.name = "Sammy";
+tindUser.isLoggedIn = false;
+//console.log(tindUser);
+
+const regular_user = {
+  email: "some@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "Aurwa",
+      lastname: "Zafar",
+    },
+  },
 };
 
-console.log(JsUser.greeting());
+//console.log(regular_user.fullname.userfullname.firstname);
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "c", 4: "d" };
+// const obj3 = { obj1, obj2 };
+const obj3 = Object.assign({}, obj1, obj2);
+console.log(obj3);
