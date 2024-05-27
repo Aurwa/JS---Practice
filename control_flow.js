4ba0b48fa5149c1c53b05a2d597cac1f2e96b817
Aurwa <zafaraurwa11@gmail.com>
2024-05-27 14:58:06 +0500

    Control Flow in JS

---
+++ control_flow.js
@@ -0,0 +1,16 @@
+// let userIsLoggedIn = true;
+// let temperature = 51;
+
+// if (temperature === 41) {
+//   console.log("Temp is less than 50");
+// } else {
+//   console.log("Temp is greater than 50");
+// }
+
+const score = 200;
+if (score > 100) {
+  const power = "fly";
+  console.log(`Power: ${power}`);
+}
+
+//console.log(power);
