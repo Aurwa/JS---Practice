ed22b8f96979b2859eb7b9b82fc83d33477d7499
Aurwa <zafaraurwa11@gmail.com>
2024-06-03 16:26:12 +0500

    keyboard_events_prac

--- Practice_JS/Async_projects/project_2/script.js
+++ Practice_JS/Async_projects/project_2/script.js
@@ -1,11 +1,82 @@
-const table = document.querySelector("table");
+// const table = document.querySelector("table");
+
+// window.addEventListener("keydown", (e) => {
+//   table.innerHTML += `
+//     <tr>
+//       <td>${e.key === " " ? "Space" : e.key}</td>
+//       <td>${e.keyCode}</td>
+//       <td>${e.code}</td>
+//     </tr>
+//   `;
+// });
+
+// using Arrays
+/*
+const tableBody = document.querySelector("#table-body");
+let keyPresses = [];
 
 window.addEventListener("keydown", (e) => {
-  table.innerHTML += `
+  keyPresses.push({
+    key: e.key,
+    keyCode: e.keyCode,
+    code: e.code,
+  });
+  renderTable();
+});
+
+function renderTable() {
+  tableBody.innerHTML = "";
+  keyPresses.forEach((keyPress) => {
+    tableBody.innerHTML += `
     <tr>
-      <td>${e.key === " " ? "Space" : e.key}</td>
-      <td>${e.keyCode}</td>
-      <td>${e.code}</td>
+      <td>${keyPress.key}</td>
+      <td>${keyPress.keyCode}</td>
+      <td>${keyPress.code}</td>
     </tr>
   `;
+  });
+}
+
+console.log(keyPresses);
+*/
+
+// Approach - 1
+/*
+let tableBody = document.querySelector("#table-body");
+
+window.addEventListener("keydown", (e) => {
+  tableBody.innerHTML += `
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+ `;
 });
+*/
+
+// Approach-2 Using Arrays
+tableBody = document.querySelector("#table-body");
+const keyPresses = [];
+
+window.addEventListener("keydown", (e) => {
+  keyPresses.push({
+    key: e.key,
+    keyCode: e.keyCode,
+    code: e.code,
+  });
+  renderTable();
+});
+
+function renderTable() {
+  tableBody.innerHTML = "";
+  keyPresses.forEach((keyPress) => {
+    tableBody.innerHTML += `
+  <tr>
+    <td>${keyPress.key}</td>
+    <td>${keyPress.keyCode}</td>
+    <td>${keyPress.code}</td>
+  </tr>
+  `;
+  });
+}
