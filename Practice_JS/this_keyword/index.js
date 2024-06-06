28a554434298f8d05b5710fcf838afdb1655f1b0
Aurwa <zafaraurwa11@gmail.com>
2024-06-06 22:24:14 +0500

    this_js

--- Practice_JS/this_keyword/index.js
+++ Practice_JS/this_keyword/index.js
@@ -1,1 +1,84 @@
-console.log(Object.__proto__);
+/*
+const thor = {
+  name: "Thor",
+  greet: function () {
+    console.log(`${this.name} says Hello!!`);
+  },
+  // arrowGreet: () => {
+  //   console.log(this);
+  //   console.log(`${this.name} says Hello!!`);
+  // },
+};
+//thor.greet();
+// thor.arrowGreet();
+
+const jade = {
+  name: "Jade",
+  friend: {
+    greet: function () {
+      console.log(this);
+      console.log(`${this.name} says hola`);
+    },
+  },
+};
+
+jade.friend.greet();
+
+// Object.prototype.greet = function () {
+//   console.log(`${this.name} says hello!`);
+//   console.log(this);
+// };
+
+// thor.greet();
+// jade.greet();
+*/
+
+// call
+
+// const jade = {
+//   name: "Jade",
+//   friend: {
+//     name: "Priya",
+//     greet: function (...args) {
+//       console.log(args);
+//       console.log(`${this.name} says hello`);
+//     },
+//   },
+// };
+
+// jade.friend.greet.call({ name: "Aurwa" });
+// jade.friend.greet.call({ name: "Mia" }, 1, 2, 3);
+
+// apply
+
+/*
+const jade = {
+  name: "Jade",
+  friend: {
+    name: "Priya",
+    greet: function (...args) {
+      console.log(args);
+      console.log(`${this.name} says hello`);
+    },
+  },
+};
+
+jade.friend.greet.apply({ name: "Aurwa" });
+jade.friend.greet.apply({ name: "Mia" }, [1, 2, 3]);
+*/
+
+function Hero(name) {
+  this.name = name;
+  this.greet = function () {
+    console.log(this);
+    console.log(`${this.name} says awesome!`);
+  };
+}
+
+const storm = new Hero("Storm");
+console.log(storm);
+
+console.log(Object.getOwnPropertyNames(storm));
+//console.log(Object.getOwnPropertyNames(storm.__proto__));
+// console.log(storm.__proto__.constructor);
+console.log(storm.__proto__.constructor.name);
